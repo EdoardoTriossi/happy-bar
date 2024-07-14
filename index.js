e94356d8499931d16ad960514bc109b4dcf08dbc
Edoardo Triossi <edoardo.triossi@studio.unibo.it>
2024-07-14 18:33:35 +0200

    corretto le ultime liste

--- index.js
+++ index.js
@@ -392,21 +392,34 @@ const React = {
     </div>
     <div class="col">
       <p>
-        Mounting Phase: constructor(): Inizializzare il componente, settare lo stato iniziale e i vincoli. componentWillMount(): Deprecato, chiamato prima che il componente venga eseguito per la prima volta.
-        render(): Eseque i componenti della UI. componentDidMount(): Invocato dopo che il componente é stato e seguito per la prima volta. Usato per effetti secondari come il recupero dei dati o interazioni con
-        il DOM.
+        Mounting Phase:
+        <ul> 
+          <li>constructor(): Inizializzare il componente, settare lo stato iniziale e i vincoli. </li>
+          <li>componentWillMount(): Deprecato, chiamato prima che il componente venga eseguito per la prima volta.</li>
+          <li>render(): Eseque i componenti della UI. </li>
+          <li>componentDidMount(): Invocato dopo che il componente é stato e seguito per la prima volta. Usato per effetti secondari come il recupero dei dati o interazioni con il DOM.</li>
+        </ul>
       </p>
     </div>
     <div class="col">
       <p>
-        Updating Phase: componentWillReceiveProps(nextProps): Deprecato, chiamato quando il componente riceve nuove proprietá prima di aggiornarsi. shouldComponentUpdate(nextProps, nextState): Determina se il
-        componete debba aggiornarsi, migliorando le prestazioni prevenendo esecuzioni inutili. componentWillUpdate(nextProps, nextState): Deprecato, chiamato prima che il componente di aggiorni. render():
-        Ricarica la UI del componente con lo stato e le proprietá aggiornate. componentDidUpdate(prevProps, prevState): Invocato dopo che il componente si a aggiorna. Usato per gli effetti secondari dopo
-        l'aggiornamento.
+        Updating Phase:         
+        <ul> 
+          <li>componentWillReceiveProps(nextProps): Deprecato, chiamato quando il componente riceve nuove proprietá prima di aggiornarsi. </li>
+          <li>shouldComponentUpdate(nextProps, nextState): Determina se il componete debba aggiornarsi, migliorando le prestazioni prevenendo esecuzioni inutili. </li>
+          <li>componentWillUpdate(nextProps, nextState): Deprecato, chiamato prima che il componente di aggiorni. </li>
+          <li>render(): Ricarica la UI del componente con lo stato e le proprietá aggiornate. </li>
+          <li>componentDidUpdate(prevProps, prevState): Invocato dopo che il componente si a aggiorna. Usato per gli effetti secondari dopo l'aggiornamento.</li>
+        </ul>
       </p>
     </div>
     <div class="col">
-      <p>Unmounting Phase: componentWillUnmount(): Invocato prima che un componente sia rimosso dal DOM. Usata per i metodi di pulizia quando rimuovono event listeners.</p>
+      <p>
+        Unmounting Phase: 
+        <ul>
+        <li>componentWillUnmount(): Invocato prima che un componente sia rimosso dal DOM. Usata per i metodi di pulizia quando rimuovono event listeners.</li>
+        </ul>
+      </p>
     </div>
   </div>
 
@@ -707,35 +720,32 @@ const Issues = {
         </div>
       </div>
       <div class="col">
-        <h3>What is the purpose of the optional chaining operator in React? Quale é l'obbiettivo dell'optional chaining operator in React?</h3>
+        <h3>Quale é l'obbiettivo dell'optional chaining operator in React?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta10" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta10">
           <p>
-            The optional chaining operator in React allows you to safely access properties of an object or elements of an array without causing an error if the object or array is undefined or null. It provides a concise
-            and convenient way to handle potential undefined values and prevent runtime errors. L'optinal chainig operator in React permette di accedere a proprietá di un oggetto o elemento di un orray senza causare un
+            L'optinal chainig operator in React permette di accedere a proprietá di un oggetto o elemento di un orray senza causare un
             errore se l'oggeto o l'array é indefinito o nullo.
           </p>
         </div>
       </div>
       <div class="col">
-        <h3>Why is it recommended to use the functional update approach when updating React states? Perché si raccomanda di usare l'approccio functional update quando si aggiornano gli stati di React?</h3>
+        <h3>Perché si raccomanda di usare l'approccio functional update quando si aggiornano gli stati di React?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta11" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta11">
           <p>
-            It is recommended to use the functional update approach when updating React states to ensure that the correct state value is referenced. By using the functional update approach, the state update is based on
-            the previous state value, preventing issues that may arise when directly updating the state with a new value. E' raccomandato l'uso del approccio functional update quando si aggiornano gli stati di React per
+            E' raccomandato l'uso del approccio functional update quando si aggiornano gli stati di React per
             assicurarsi che si riferisca al valore corretto dello stato. Usando l'approccio functional update l'aggiornamento dello stato si basa sullo valore dello stato precedente, prevenendo problemi che potrebbero
             sorgere quando si aggiorna dirrettamente lo stato con un nuovo valore.
           </p>
         </div>
       </div>
       <div class="col">
-        <h3>What is the purpose of React hooks in handling states? Qualo é lo scopo dei React hooks nella gestione degli stati?</h3>
+        <h3>Qualo é lo scopo dei React hooks nella gestione degli stati?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta12" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta12">
           <p>
-            React hooks, specifically useState, make it easy to handle states in React by allowing the creation of a state variable, its set function, and the initial state inside the useState hook. The set function is
-            then used to update the state. I React hooks, in particolare useState, rendono facile la gestione degli stati in React perché permettono la creazione di una variabile di stato, la sua set function, e lo stato
+            I React hooks, in particolare useState, rendono facile la gestione degli stati in React perché permettono la creazione di una variabile di stato, la sua set function, e lo stato
             iniziale dentro lo useState hook. La set function é poi usata per aggiornare lo stato.
           </p>
         </div>
@@ -743,48 +753,44 @@ const Issues = {
     </div>
     <div class="row">
       <div class="col">
-        <h3>How can you update multiple properties of a React state object efficiently? Come si aggiornano efficentemente multiple proprietá di un oggetto di stato in React?</h3>
+        <h3>Come si aggiornano efficentemente multiple proprietá di un oggetto di stato in React?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta13" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta13">
           <p>
-            To update multiple properties of a React state object efficiently, you can use the spread operator to create a new state object that includes all the properties from the previous state, and then update only
-            the desired properties. This allows for a concise and readable way to update multiple properties without directly modifying the original state object. Per aggiornare efficentemente multiple proprietá di un
+            Per aggiornare efficentemente multiple proprietá di un
             oggetto di stato in React si puó usare lo spread operator per creare un nuovo oggetto di stato che includa tutte le prioritá dello stato precedente e poi aggiornare solo le proprietá desiderate. Questo
             permette di aggiornare multiple proprietá senza modificare direttamente lo state object originale in modo leggibile e coinciso.
           </p>
         </div>
       </div>
       <div class="col">
-        <h3>What is the potential issue with directly updating a React state without returning an object? Quale é il problema potenziale dell'aggiornare direttamente uno stato in React senza ritornare un oggetto?</h3>
+        <h3>Quale é il problema potenziale dell'aggiornare direttamente uno stato in React senza ritornare un oggetto?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta14" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta14">
           <p>
-            The potential issue with directly updating a React state without returning an object is that the state will be updated with the new value, but it will lose all other properties and become just the new value.
-            To ensure that the state retains its other properties, it is important to return an object that includes the previous state properties along with the updated property. Il problema potenziale del aggiornare
+            Il problema potenziale del aggiornare
             direttamente uno stato di React senza ritornare un oggetto é che lo stato sará aggiornato con il nuovo valore, ma perderá tutte le altre proprietá e diventerá solamete il nuovo valore. Per assicurarsi che lo
             stato mantenga le sue altre proprietá é importante ritornare un oggetto che includa le proprietá dello stato precedente assieme alle proprietá aggiornate.
           </p>
         </div>
       </div>
       <div class="col">
-        <h3>Why is it beneficial to use a single useState hook for multiple inputs in React? Perché é vantagioso usare un singolo useState hook per input multipli in React?</h3>
+        <h3>Perché é vantagioso usare un singolo useState hook per input multipli in React?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta15" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta15">
           <p>
-            Using a single useState hook for multiple inputs in React reduces code duplication and improves code readability. Instead of creating separate useState hooks for each input, you can manage the state of all
-            inputs using a single state object and update the desired properties based on the input values. This approach simplifies the code and makes it easier to handle multiple inputs. Usare un singolo useState hook
+            Usare un singolo useState hook
             per input multipli in React riduce la duplicazione del codice e migliora la leggibilitá. Invece di creare useState hooks separati per ogni input, si puó gestire lo stato di tutti gli input usando un singolo
             state object e aggiornando le proprietá desiderate sulla base dei valori di input. Questo approccio semplifica il codice e rende piú facile gestire input multipli.
           </p>
         </div>
       </div>
       <div class="col">
-        <h3>When should you consider using useReducer instead of useState in React? Quando si dovrebbe considerare l'uso di useReducer invece di useState in React?</h3>
+        <h3>Quando si dovrebbe considerare l'uso di useReducer invece di useState in React?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta16" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta16">
           <p>
-            You should consider using useReducer instead of useState in React when you have a more complex state object with multiple properties and actions. useReducer provides a more structured and organized way To
-            handle state updates and actions, making it easier to manage and maintain the state in a larger application. Bisognerebbe considerare l'uso di useReducer invece di useState in React quando si ha uno state
+            Bisognerebbe considerare l'uso di useReducer invece di useState in React quando si ha uno state
             object piú complesso con proprietá e azioni multiple. useReducer fornisce un modo piú stritturato e organizzato per gestire aggiornamenti e azioni di stato, rendendo piú facile la gestione e il mantenimento
             dello stato in aplicazioni piú grandi.
           </p>
@@ -793,12 +799,11 @@ const Issues = {
     </div>
     <div class="row">
       <div class="col">
-        <h3>What is the importance of initializing states correctly in React? Quale é l'importanza di inizializzare stati correttamente in React?</h3>
+        <h3>Quale é l'importanza di inizializzare stati correttamente in React?</h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta17" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta17">
           <p>
-            Initializing states correctly is important in React to prevent issues such as blank pages or undefined values. It is recommended to initialize states with the expected type and provide default values if
-            necessary. This helps ensure that the state is properly initialized and avoids potential errors when accessing state properties. Inizializzare stati correttamente in React é importante per prevenire problemi
+            Inizializzare stati correttamente in React é importante per prevenire problemi
             come pagine bianche o valori indefiniti. E' ravvomandato inizializzare stati con il tipo aspettato e fornire valori di default se necessario. Questo aiuta ad assicurarsi che lo stato é inizializzato
             correttamente e evita potenziali errori quando si accede alle proprietá dello stato.
           </p>
@@ -806,17 +811,15 @@ const Issues = {
       </div>
       <div class="col">
         <h3>
-          What is the potential issue with storing a new object instead of the id when deriving states in React? Quale é il potenziale problema dell'immagazzinare un nuovo oggetto invece dell'id quando si derivano gli
+          Quale é il potenziale problema dell'immagazzinare un nuovo oggetto invece dell'id quando si derivano gli
           stati in React?
         </h3>
         <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#risposta18" aria-expanded="false" aria-controls="risposta">Risposta</button>
         <div class="collapse" id="risposta18">
           <p>
-            The potential issue with storing a new object instead of the id when deriving states in React is that the derived state may not reflect the changes made to the original state. This can lead to inconsistencies
-            and unexpected behavior when updating the derived state. To ensure that the derived state accurately reflects the changes, it is recommended to store the id of the object and retrieve the object using the id
-            when needed. Il potenziale problema dell'immagazzinare un nuovo oggetto invece dell'id quando si derivano stati in React é che gli stati derivati potrebbero non riflettere i cambiamneti fatti allo stato
+            Il potenziale problema dell'immagazzinare un nuovo oggetto invece dell'id quando si derivano stati in React é che gli stati derivati potrebbero non riflettere i cambiamneti fatti allo stato
             originale. questo puó portare a inconsistenze e comportamenti inaspettati quando si aggiorna lo stato derivato. Per assicurarsi che lo stato derivato rifletta accuratamente i cambiamenti é raccomandato di
-            immagazzinare l'id dell'oggetto e recuperare l'oggetto usando l'id quuando serve.
+            immagazzinare l'id dell'oggetto e recuperare l'oggetto usando l'id quando serve.
           </p>
         </div>
       </div>
